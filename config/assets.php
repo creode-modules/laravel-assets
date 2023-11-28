cdae2a11ef0dcd2e4a3d648dd06eb98519e2910d
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-11-28 17:10:15 +0000

    Move image mime types from model class into config.

--- config/assets.php
+++ config/assets.php
@@ -13,4 +13,22 @@ return [
     */
 
     'disk' => env('FILESYSTEM_DISK', 'public'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Image Mime Types
+    |--------------------------------------------------------------------------
+    |
+    | A list of mime types that are considered to be images. This is used when
+    | determining whether to render an asset as an image.
+    |
+    */
+
+    'image_mime_types' => [
+        'image/jpeg',
+        'image/png',
+        'image/gif',
+        'image/tif',
+        'image/webp',
+    ],
 ];
