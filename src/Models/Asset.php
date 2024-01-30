3426153859a54a1c2eacfb075aa1c31e831f7292
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-01-30 12:07:17 +0000

    Adds disk field so that if disks need to switch, they can still work as expected.

--- src/Models/Asset.php
+++ src/Models/Asset.php
@@ -11,26 +11,60 @@ class Asset extends Model
 {
     use HasFactory;
 
+    /**
+     * Sets the table for all models extending this model.
+     *
+     * @var string
+     */
     protected $table = 'assets';
 
+    /**
+     * Perform any actions required after the model boots.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::deleting(function ($asset) {
+            Storage::disk($asset->disk)->delete($asset->location);
+        });
+    }
+
+    /**
+     * Gets the path to the asset.
+     *
+     * @return Attribute
+     */
     public function path(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, $attributes) => Storage::disk(config('assets.disk'))
+            get: fn ($value, $attributes) => Storage::disk($this->disk)
                 ->path($attributes['location']),
         );
     }
 
+    /**
+     * Gets the assets url.
+     *
+     * @return Attribute
+     */
     public function url(): Attribute
     {
         return Attribute::make(
-            get: fn ($value, $attributes) => Storage::disk(config('assets.disk'))
+            get: fn ($value, $attributes) => Storage::disk($this->disk)
                 ->url($attributes['location']),
         );
     }
 
-    public function isImage($asset): bool
+    /**
+     * Determines if the asset is an image.
+     *
+     * @param string $mimeType
+     *
+     * @return boolean
+     */
+    public function isImage(string $mimeType): bool
     {
-        return in_array($asset, config('assets.image_mime_types'));
+        return in_array($mimeType, config('assets.image_mime_types'));
     }
 }
